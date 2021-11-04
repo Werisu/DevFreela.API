fafe25d9d57e1bdad5fdcca9380a1ca2d309b961
TI <ti@ARN-LAB04-PC23.ifto.local>
2021-11-03 22:21:08 -0300

    implementando create e update exemplo

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -1,3 +1,4 @@
+using DevFreela.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers
@@ -10,9 +11,56 @@ namespace DevFreela.API.Controllers
     /// </summary>
     public class ProjectsController : ControllerBase
     {
+        // api/projects?query=net core
         [HttpGet]
-        public IActionResult Get(){
+        public IActionResult Get(string query)
+        {
+            // buscar todos ou filtrar
             return Ok();
+        }
+
+        // api/projects/3
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // buscar o projeto
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if(createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            //201 - Cadastrar o projeto
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        // api/projects/2
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if(updateProject.Description.Length < 5)
+            {
+                return BadRequest();
+            }
+
+            // Atualizo o objeto 
+            return NoContent();
+        }
+
+        // api/projects/1
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Buscar, se não existir, retorna notfound
+
+            // Remover
+
+            return NoContent();
         }
     }
 }
